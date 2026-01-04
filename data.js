ae3de8d1be45706e7e1f508f6862029ad64e9aa6
CarolGP <cgpardal@gmail.com>
2026-01-04 19:59:47 +0100

    ej sp9

---
+++ data.js
@@ -0,0 +1,7 @@
+const webPageInfo = {
+    title: "Mi página molona",
+    subtitle: "Esta página va a ganar un pastizal",
+    description: "No se de que va a ir pero lo va a petar"
+}
+
+module.exports = webPageInfo
